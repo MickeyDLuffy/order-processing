c589e7636c20f19b139e15a52738708baffc45fd
MickeyDLuffy <thefalconslayer@gmail.com>
2021-12-01 16:22:32 +0000

    starting project

---
+++ src/main/java/com/redbrokers/orderprocessing/restcontroller/OrderRestController.java
@@ -0,0 +1,21 @@
+package com.redbrokers.orderprocessing.restcontroller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/order")
+public class OrderRestController {
+
+    @GetMapping("/")
+    public ResponseEntity<?> process() {
+        return ResponseEntity.created(URI.create("")).body("");
+    }
+}
